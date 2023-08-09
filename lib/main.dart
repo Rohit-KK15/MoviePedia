f1218f35cd9fe5339699a56fe5024a4b9750c8dc
Rohit-KK15 <105338950+Rohit-KK15@users.noreply.github.com>
2023-08-09 22:33:52 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:project2/Screens/homeScreen.dart';
 import 'package:project2/Screens/search.dart';
 import 'package:project2/Screens/watchLater.dart';
@@ -31,18 +32,43 @@ class MyApp extends StatelessWidget {
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
+
+
   @override
   State<Home> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
 
+  int selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: HomeScreen()
     );
   }
+// void onItemTapped(int index) {
+//   setState(() {
+//     selectedIndex = index;
+//   });
+//   String lbl;
+//   if(selectedIndex==0) {
+//     lbl="Home";
+//   } else if(selectedIndex==1) {
+//     lbl="Search";
+//   } else {
+//     lbl="Watch Later";
+//   }
+//   print(index);
+//   if(page!=lbl && index==0) {
+//     Navigator.pushReplacement(context,MaterialPageRoute(builder: (context) =>const MyApp()));
+//   } else if(page!=lbl && index==1) {
+//     Navigator.pushReplacement(context,MaterialPageRoute(builder: (context) =>const SearchPage()));
+//   } else if(page!=lbl && index==2) {
+//     Navigator.pushReplacement(context,MaterialPageRoute(builder: (context) =>const watchLater()));
+//   }
+// }
 
 }
