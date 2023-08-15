08eb0ae5c7679d56453bf418a823832b8fbc5795
Rohit-KK15 <105338950+Rohit-KK15@users.noreply.github.com>
2023-08-15 22:30:26 +0530

    Create movie_model.dart

---
+++ lib/model/movie_model.dart
@@ -0,0 +1,51 @@
+class Movie {
+  int id;
+  String name;
+  String desc;
+  String bannerUrl;
+  String posterUrl;
+  String vote;
+  String launchOn;
+  List<dynamic> cast;
+  List<dynamic> crew;
+
+  Movie({
+    required this.id,
+    required this.name,
+    required this.desc,
+    required this.bannerUrl,
+    required this.posterUrl,
+    required this.vote,
+    required this.launchOn,
+    required this.cast,
+    required this.crew,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'desc': desc,
+      'bannerUrl': bannerUrl,
+      'posterUrl': posterUrl,
+      'vote': vote,
+      'launchOn': launchOn,
+      'cast': cast != null ? cast.join(',') : null,
+      'crew': crew != null ? crew.join(',') : null,
+    };
+  }
+
+  factory Movie.fromMap(Map<String, dynamic> map) {
+    return Movie(
+      id: map['id'],
+      name: map['name'],
+      desc: map['desc'],
+      bannerUrl: map['bannerUrl'],
+      posterUrl: map['posterUrl'],
+      vote: map['vote'],
+      launchOn: map['launchOn'],
+      cast: map['cast'] != null ? map['cast'].split(',') : null,
+      crew: map['crew'] != null ? map['crew'].split(',') : null,
+    );
+  }
+}
